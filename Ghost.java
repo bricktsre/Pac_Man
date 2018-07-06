daf9b63177425a8d679cedcabb4a7e632e308805
Greg <Greg@WOODWARD-GREG>
2018-07-06 00:17:48 -0400

    Rudimentay Move and Draw Methods
    Has a 70% chance of staying the same direction, other 3 directions split
    into 10% chances each
    Draws a oval of the color of the ghost

--- Ghost.java
+++ Ghost.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Ghost extends Character{
 	private Color c;	//Color of the ghost
@@ -6,5 +7,21 @@ public class Ghost extends Character{
 	public Ghost(CardinalDirection a, int initialdx, int initialdy, int w, Color ghostcolor) {
 		super(a,initialdx,initialdy, w);
 		c=ghostcolor;
+	}
+	
+	public void changeDirection() {
+		ArrayList<CardinalDirection> b = new ArrayList<CardinalDirection>();
+		b.add(CardinalDirection.UP);
+		b.add(CardinalDirection.RIGHT);
+		b.add(CardinalDirection.LEFT);
+		b.add(CardinalDirection.DOWN);
+		b.remove(d);
+		int a = (int)(Math.random()*11);
+		if(a==7)
+			d=b.get(0);
+		else if(a==8)
+			d=b.get(1);
+		else
+			d=b.get(2);
 	}
 }
