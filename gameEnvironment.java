15685fcd53f4761c8d21e06a39d02f381a54bdda
Greg <Greg@WOODWARD-GREG>
2018-07-06 22:35:28 -0400

    Finished death method and changed organization
    Finished the death part of pointsDeaths by adding in the reset
    capabilities
    Created a character array to handle methods that need to run through all
    characters in the game.

--- gameEnvironment.java
+++ gameEnvironment.java
@@ -5,8 +5,9 @@ public class gameEnvironment {
 	private GameSquare[][] gsquare;				//2D array of all the squares making up the map
 	private pacman pman;						//Pacman character
 	private Ghost [] ghosts = new Ghost[1];		//Array of ghosts
-	private int height;
-	private int width;
+	private Character[] car;					//Array of pacman and ghost characters
+	private int height;							//Number of GameSquares high the map is
+	private int width;							//Number of GameSquares wide the map is
 	
 	public gameEnvironment(MapReader m) {
 		int[] a = m.getHeightWidth();
@@ -16,8 +17,12 @@ public class gameEnvironment {
 		initializeBoard(m);
 		a = m.getStartCoordinates();
 		pman = new pacman(CardinalDirection.LEFT,a[0],a[1],width);
-		for(int i =0;i<ghosts.length;i++)
+		car = new Character[1+ghosts.length];
+		car[0]=pman;
+		for(int i =0;i<ghosts.length;i++) {
 			ghosts[i]= new Ghost(CardinalDirection.NONE,36,36,width,Color.GREEN);
+			car[i+1]=ghosts[i];
+		}	
 	}
 	
 	//Initializes the playing board with a border of walls and wall blocks randomly inside with a bias
@@ -63,11 +68,11 @@ public class gameEnvironment {
 	/*Test to see if a character moving would cause it to go into a wall
 	 * If it is going to go into a wall then it returns false
 	 */
-	private boolean checkWallCollision(Character car) {
-		int a = car.getSquareIn();
-		int row = a/height+car.getDirection().dy;
-		int col = a%width+car.getDirection().dx;
-		if(gsquare[row][col].isWall()&&(car.atCenterLineC(gsquareIn(car), car.getDirection())==0))
+	private boolean checkWallCollision(Character c) {
+		int a = c.getSquareIn();
+		int row = a/height+c.getDirection().dy;
+		int col = a%width+c.getDirection().dx;
+		if(gsquare[row][col].isWall()&&(c.atCenterLineC(gsquareIn(c), c.getDirection())==0))
 			return false;
 		return true;			
 	}
@@ -92,10 +97,10 @@ public class gameEnvironment {
 	
 	//Moves the character in a diagonal fashion
 	//Checks to see if the diagonal move is complete and if it is changes the diagonal direction something to NONE
-	private void diagonalMove(pacman car) {
-		car.moveDiag();
-		if(car.atCenterLineD(gsquareIn(car))==0)
-			car.changeDiagDir(DiagDirection.NONE);
+	private void diagonalMove(pacman p) {
+		p.moveDiag();
+		if(p.atCenterLineD(gsquareIn(p))==0)
+			p.changeDiagDir(DiagDirection.NONE);
 	}
 	
 	//Returns the GameSquare the Character a is currently in
@@ -107,12 +112,13 @@ public class gameEnvironment {
 	 * 
 	 */
 	private void pointsDeaths() {
-		/*for(Ghost a: ghosts) {
+		for(Ghost a: ghosts) {
 			if(gsquareIn(a)==gsquareIn(pman)){
 				pman.loseLife();
-				//reset positions
+				for(Character c: car)
+					c.resetPosition();
 			}
-		}*/
+		}
 		if(gsquareIn(pman).hasPoint()) {
 			pman.increaseScore(10);
 			gsquareIn(pman).removePoint();
@@ -126,8 +132,7 @@ public class gameEnvironment {
 			for(int j =0;j<gsquare[0].length;j++)
 				gsquare[i][j].draw(g, i, j);
 		}
-		pman.draw(g);;
-		for(Ghost a: ghosts)
+		for(Character a: car)
 			a.draw(g);
 			
 	}
