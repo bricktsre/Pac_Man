008a745675c647a3a6ccd39f435d753fd4f36a1a
Greg <Greg@WOODWARD-GREG>
2018-07-28 21:08:44 -0400

    Direction enumerator

---
+++ Direction.java
@@ -0,0 +1,22 @@
+public enum Direction {
+
+	UP(0, -1), DOWN(0, 1), LEFT(-1, 0), RIGHT(1, 0),  NONE(0, 0);
+	
+	public int dx,dy;
+	
+	private Direction(int dx, int dy) {
+		this.dx = dx;
+		this.dy = dy;
+	}
+	
+	public Direction opposite() {
+		switch(this) {
+			case UP: return Direction.DOWN;
+			case RIGHT: return Direction.LEFT;
+			case DOWN: return Direction.UP;
+			case LEFT: return Direction.RIGHT;
+			default: break;
+		}
+		return Direction.NONE;
+	}
+}
